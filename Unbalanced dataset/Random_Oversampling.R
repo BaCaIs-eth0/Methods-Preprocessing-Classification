1b5c7f0e76be41771aa2dd3866a09afb25e43d4e
Carlos Jesus Fernandez Basso <cjferba@gmail.com>
2015-04-20 19:36:08 +0200

    ROS

---
+++ Unbalanced dataset/Random_Oversampling.R
@@ -0,0 +1,42 @@
+
+
+# Distance function
+distance <- function(i, j, data){
+  sum <- 0
+  for(f in 1:dim(data)[2]){
+    if(is.factor(data[,f])){ # nominal feature
+      if(data[i,f] != data[j,f]){
+        sum <- sum + 1
+      }
+    } else {
+      sum <- sum + (data[i,f] - data[j,f]) * (data[i,f] - data[j,f])
+    }
+  }
+  sum <- sqrt(sum)
+  return(sum)
+}
+#random oversampling
+ROS<-function(datos,semilla=0){
+  if(semilla!=0){
+    set.seed(semilla)
+  }
+  tipo <- unique(datos[,dim(datos)[2]])
+  vec1<- datos[datos[,dim(datos)[2]]==tipo[1],]
+  vec2<- datos[datos[,dim(datos)[2]]==tipo[2],]
+  
+  diff<-abs((dim(vec1)[1]-dim(vec2)[1]))
+   
+  if(dim(vec1)[1]>dim(vec2)[1]){
+    posiciones<-sample(dim(vec2)[1],diff,replace = T)
+    elementos<-vec2[posiciones,]
+  }else{
+    posiciones<-sample(dim(vec1)[1],diff,replace = T)
+    elementos<-vec1[posiciones,]
+  }
+  dat<-rbind(vec1,vec2,elementos)
+}
+
+
+
+
+
